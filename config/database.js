96cc8784480eb31bbbccee1f9634b4d738f674ff
hoiresz <hoireszhu@hotmail.com>
2017-08-07 10:36:53 -0700

    initial setup without mongodb username and pwd

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+
+  'url': 'mongodb://<dbuser>:<dbpassword>@ds163681.mlab.com:63681/pourover'
+}
---
+++ server.js
@@ -0,0 +1,33 @@
+let express = require('express');
+let app = express();
+let port = process.env.PORT || 3000;
+let mongoose = require('mongoose');
+let passport = require('passport');
+let flash = require('connect-flash');
+
+let morgan = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+let session = require('express-session');
+
+let configDB = require('./config/database.js');
+
+mongoose.connect(configDB.url);
+
+// require('./config/passport')(passport);
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser());
+
+app.set('view engine', 'ejs');
+
+app.use(session({ secret: 'ilovebcrypt'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+require('./app/routes.js')(app, passport);
+
+app.listen(port);
+console.log('The magic happens on port' + port);
