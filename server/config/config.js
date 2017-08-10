59aa16ec39f65e724df5e8aa90df8db5d60119ef
Jstuff36 <mizzou_fan10@hotmail.com>
2017-08-10 09:52:01 -0700

    Remove unnecessary code

--- server/config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	db: 'mongodb://xiaoyuanz:Jstuff36!@ds163681.mlab.com:63681/pourover',
-	secret: 'IloveBcrypt'
-};
--- server/controllers/home.js
+++
@@ -1,7 +0,0 @@
-const User = require('../models/user');
-const jwt = require('jsonwebtoken');
-
-exports.getHomePage = (req, res, next) => {
-	console.log(req.user);
-	res.send({ message: 'Home page' });
-}
