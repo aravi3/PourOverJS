10b09f53eb2db243f456ae587d8ba2ac41a9bdc9
hoiresz <hoireszhu@hotmail.com>
2017-08-11 18:44:09 -0700

    working code files directory with modals, working post code

---
+++ frontend/components/code_input/save_modal.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class SaveModal extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default SaveModal;
